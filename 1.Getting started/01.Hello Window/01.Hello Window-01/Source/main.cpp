c0cdb3fe9c0d289a2f8ce4df126bd9e610b53ed7
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-15 08:32:07 +0900

    OpenGL 버전을 3.3에서 4.6으로 변경

--- 1.Getting started/01.Hello Window/01.Hello Window-01/Source/main.cpp
+++ 1.Getting started/01.Hello Window/01.Hello Window-01/Source/main.cpp
@@ -8,8 +8,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 int main(void)
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
