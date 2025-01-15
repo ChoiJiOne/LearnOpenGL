2a4084f6648231d53b722d70c2b1707b47baba70
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-15 10:14:30 +0900

    02.Hello Triangle 실습 코드 추가

--- 1.Getting started/02.Hello Triangle/02.Hello Triangle-02/Source/main.cpp
+++ 1.Getting started/02.Hello Triangle/02.Hello Triangle-02/Source/main.cpp
@@ -87,21 +87,33 @@ int main(void)
 
 	std::vector<float> vertices =
 	{
-		-0.5f, -0.5f, 0.0f,
+		+0.5f, +0.5f, 0.0f,
 		+0.5f, -0.5f, 0.0f,
-		+0.0f, +0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, +0.5f, 0.0f,
+	};
+
+	std::vector<unsigned int> indices =
+	{
+		0, 1, 3,
+		1, 2, 3,
 	};
 
 	unsigned int vbo;
 	unsigned int vao;
+	unsigned int ebo;
 
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
 
 	glBindVertexArray(vao);
 	{
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * static_cast<uint32_t>(vertices.size()), vertices.data(), GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * static_cast<uint32_t>(indices.size()), indices.data(), GL_STATIC_DRAW);
 
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0));
 		glEnableVertexAttribArray(0);
@@ -119,7 +131,7 @@ int main(void)
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
@@ -127,6 +139,7 @@ int main(void)
 	}
 
 	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &ebo);
 	glDeleteBuffers(1, &vbo);
 	glDeleteProgram(shaderProgram);
 
